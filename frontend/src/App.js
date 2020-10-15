ae67a667eadf7c65d3e8dc0f0e7e94ced472aba4
Dennis Idowu <dennis_idowu@live.com>
2020-10-14 20:53:24 -0400

    added header comp to app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Header from './components/Header.js';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
      <h1>Welcome to Static's Electronics</h1>
+     <Header/>
     </div>
   );
 }
